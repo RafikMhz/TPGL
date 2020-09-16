acad1f014f3fecddf02fe7464504ead948fe1056
RafikMhz <rafikboushaki71@gmail.com>
2020-09-16 15:47:20 +0200

    premier commit

---
+++ Orange.java
@@ -0,0 +1,77 @@
+/**
+ *
+ * @author roudet
+ */
+public class Orange {
+    private double prix;
+    private String origine;
+	
+    public Orange() 
+    {
+        this.prix = 0.5;  //prix en euros
+        this.origine="Espagne";
+    }
+    
+    public Orange(double prix, String origine) 
+    {
+	if(prix < 0)
+	    this.prix = -prix;  //une solution possible pour interdire les prix negatifs
+	else
+	    this.prix = prix;
+
+	if(origine.equals(""))
+            this.origine = "Espagne";  //Espagne par défaut
+	else
+            this.origine = origine;   
+    }
+
+    public double getPrix(){
+	return prix;
+    }
+
+    public void setPrix(double prix){
+	this.prix=prix;
+    }
+
+    public String getOrigine(){
+	return origine;
+    }
+ 
+    public void setOrigine(String origine){
+	this.origine=origine;
+    }
+
+    @Override
+    public String toString(){
+        return "Orange de " + origine + " a " + prix + " euros";
+    }
+
+    @Override
+    public boolean equals(Object o){  //predicat pour tester si 2 oranges sont equivalentes
+        if(o != null && getClass() == o.getClass()){
+            Orange or = (Orange) o;
+            return (prix == or.prix && origine.equals(or.origine));
+        }
+        return false;
+    }
+
+    public boolean isSeedless() {  //predicat indiquant qu'une orange a des pepins
+        return false;
+    }
+
+
+    public static void main (String[] args){
+        //Ecrire ici vos tests
+		Orange test = new Orange(-2,"");
+		System.out.println(test.toString());
+		Orange O1 = new Orange(0.4, "Algérie");
+		System.out.println(O1.getPrix());
+		O1.setPrix(0.2);
+		System.out.println(O1.toString());
+		System.out.println(O1.equals(test));
+		System.out.println(O1.equals(O1));
+
+
+	System.out.println("premier test Orange");
+   }
+}
